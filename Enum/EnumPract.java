d34ed3934026af3096337c7289ed18f1f818856d
Bhavim09 <bmehta@argusoft.com>
2023-12-13 16:20:22 +0530

    enum pract

---
+++ Enum/EnumPract.java
@@ -0,0 +1,20 @@
+enum check
+{
+  MONDAY(1),
+  TUESDAY(2),
+  WEDNESDAY(3);
+  
+  public int x;
+
+  private check(int value)
+  {
+    this.x  =  value;
+  }
+  
+   
+}
+public class EnumPract {
+    public static void main(String[] args) {
+        System.out.println(check.TUESDAY);
+    }
+}
